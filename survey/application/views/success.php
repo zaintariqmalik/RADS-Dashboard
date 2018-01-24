488dddd8c25b4f759abd4fc1b837569dbfbaa024
waqasahmad0 <ahmadwaqas1390@gmail.com>
2018-01-24 15:55:40 +0500

    Map Demo
    This is an increment in login.
    Map module is added.
    when we login we see a button on success page. "Show Map"
    when we press the button a controller is called which fetch data from locations table.
    and passes this data to view which displays map and mark some points.  we get latitude and langtiude from model.

---
+++ survey/application/views/success.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+    <title>My Form</title>
+</head>
+<body>
+
+<table>
+    <tr>
+        <th>Question</th>
+    </tr>
+    <?php foreach($fetch_data as $row){?>
+        <tr>
+            <td><a href="<?php echo site_url('DisplayController/getQuestionData?var1='.$row->id) ?>"><?php echo $row->question;?></a></td>
+        </tr>
+    <?php }?>
+    </table>
+       <a  href="<?php echo site_url('DisplayMapController') ?>"> <button> Show Map</button>   </a>
+
+</body>
+</html>
